276e4229e95eec1e6d4595c5052849d67c6f0d50
spell-landon <spell.landon@gmail.com>
2022-01-25 10:18:08 -0600

    Scaffold StoryFeed

--- src/components/StoryCard/StoryCard.js
+++ src/components/StoryCard/StoryCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from './StoryCard.module.css';
 
-function StoryCard(props) {
+function StoryCard({ story }) {
   return (
-    <div>
-      <p>I'm the Story Card</p>
+    <div className={styles.cardContainer}>
+      <div className={styles.textContainer}>
+        <h5>{story.author}</h5>
+        <p>{story.title}</p>
+        <span>{story.time}</span>
+      </div>
+      <img src={story.photo_url} alt={story.title} />
     </div>
   );
 }
--- src/components/StoryFeed/StoryFeed.js
+++ src/components/StoryFeed/StoryFeed.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// components
+import StoryCard from '../StoryCard/StoryCard';
+// stylesheet
 import styles from './StoryFeed.module.css';
 
 function StoryFeed(props) {
+  const [stories, setStories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // timeout for fetching the data
+    const handleLoadingTimeOut = setTimeout(() => {
+      if (!stories.length) {
+        setLoading(false);
+      }
+    }, 5000);
+    // Axios request
+    fetch('url')
+      .then((res) => res.json())
+      .then((res) => {
+        setStories(res);
+      });
+    // clear timeout
+    return () => clearTimeout(handleLoadingTimeOut);
+  });
+
+  if (loading && !stories.length) {
+    return <h2>Loading...</h2>;
+  }
+  if (!loading && !stories.length) {
+    return <h2>Something went wrong</h2>;
+  }
+
   return (
-    <div>
-      <p>I'm the Story Feed</p>
+    <div className={styles.feedContainer}>
+      <h4>Stories</h4>
+      <ul>
+        {stories.map((story) => (
+          <StoryCard key={story._id} story={story} />
+        ))}
+      </ul>
     </div>
   );
 }
