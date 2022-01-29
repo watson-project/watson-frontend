8c61e867c17b43bda10f61d2ce10c1dcc961f8c8
spell-landon <spell.landon@gmail.com>
2022-01-28 19:35:30 -0600

    Start navigation change

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Menu(props) {
+	return <div>I'm the menu</div>;
+}
+
+export default Menu;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,34 +1,64 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navigation.module.css';
 // logo
 import logo from '../../assets/logo-small.png';
 import { NavLink } from 'react-router-dom';
+import { FiMenu } from 'react-icons/fi';
 
 function Navigation(props) {
-  return (
-    <div className={styles.navContainer}>
-      <div className={styles.linkContainer}>
-        <Link to='/' className={styles.heroimg}>
-          <img src={logo} alt='company logo' />
-        </Link>
-        <div className={styles.links}>
-          <Link to='/' className={styles.addLink}>
-            Home
-          </Link>
-          <Link to='/stories' className={styles.addLink}>
-            Dashboard
-          </Link>
-          <Link to='/about' className={styles.addLink}>
-            About
-          </Link>
-          <Link to='/add-story' className={styles.addLink}>
-            Add Story
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+	const [menu, setMenu] = useState(false);
+
+	const toggleMenu = () => {
+    setMenu(!menu);
+		return (
+			<div className={styles.links}>
+				<Link to='/' className={styles.addLink}>
+					Home
+				</Link>
+				<Link to='/stories' className={styles.addLink}>
+					Dashboard
+				</Link>
+				<Link to='/about' className={styles.addLink}>
+					About
+				</Link>
+				<Link to='/add-story' className={styles.addLink}>
+					Add Story
+				</Link>
+			</div>
+		);
+	};
+
+	return (
+		<div className={styles.navContainer}>
+			<div className={styles.linkContainer}>
+				<Link to='/' className={styles.heroimg}>
+					<img src={logo} alt='company logo' />
+				</Link>
+				{/* {menu ? (
+					<div className={styles.links}>
+						<Link to='/' className={styles.addLink}>
+							Home
+						</Link>
+						<Link to='/stories' className={styles.addLink}>
+							Dashboard
+						</Link>
+						<Link to='/about' className={styles.addLink}>
+							About
+						</Link>
+						<Link to='/add-story' className={styles.addLink}>
+							Add Story
+						</Link>
+					</div>
+				) : (
+					''
+				)}
+				<FiMenu onClick={toggleMenu} /> */}
+				<FiMenu onClick={toggleMenu} />
+			</div>
+		</div>
+	);
 }
 
 export default Navigation;
